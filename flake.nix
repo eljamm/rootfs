d42e3942eee4fdd61e85fd6a6866fdb211faf691
eljamm <fedi.jamoussi@protonmail.ch>
2026-04-28 10:18:18 +0200

    feat: add initial packages for felix

--- flake.nix
+++ flake.nix
@@ -54,7 +54,23 @@
             packages = with pkgs; [
               bash
               coreutils
-              hello
+
+              glibc_multi.static
+
+              sudo
+              file
+              lsof
+
+              # rsync
+              # dbus
+              # systemd
+              # pulseaudio
+
+              # libglvnd
+              # mesa
+              # mesa_glu
+
+              # wine # 32-bit & 64-bit on x86_64-linux, else 32-bit
             ];
           };
           hello-ubuntu = {
