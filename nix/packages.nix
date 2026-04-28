6a705a6a23ed3f2125c4622fb47fe883af39a94a
eljamm <fedi.jamoussi@protonmail.ch>
2026-04-28 12:38:27 +0200

    feat: add more packages to rootfs

--- nix/packages.nix
+++ nix/packages.nix
@@ -2,20 +2,65 @@ pkgs: with pkgs; [
   bash
   coreutils
 
-  glibc_multi.static
-
+  # system
   sudo
   file
   lsof
-
+  polkit
   rsync
+  lsb-release
+  glibc_multi.static
   dbus
   systemd
-  pulseaudio
+  glibcLocales
+  fuse2
+  openssl
+  curl
+  xz
+  python3
 
-  libglvnd
+  # audio
+  pulseaudio
+  openal
+  speex
+  libvorbis
+  alsa-plugins
+
+  # multimedia
+  SDL
+  SDL2
+  libjpeg_turbo
+  harfbuzz
+
+  # graphics
   mesa
   mesa_glu
+  libglvnd
+  glew
+  mesa-demos
+  vulkan-tools
+  libvdpau
+  libva
+  # removed from Nixpkgs (obsolete)
+  #llvm_14
+  #llvm_16
+  llvm
+  clinfo
 
+  # gaming
+  mangohud
   wine # 32-bit & 64-bit on x86_64-linux, else 32-bit
+
+  # other
+  protobuf
+  protobufc
+  libxcb
+  xcb-util-cursor
+  xcb-imdkit
+  libxcb-image
+  libxcb-keysyms
+  libxcb-wm
+  libxkbcommon
+
+  # TODO: 32-bit
 ]
