18042375bed57784bd86445e1e630b682743a53c
eljamm <fedi.jamoussi@protonmail.ch>
2026-05-04 10:42:41 +0200

    feat: add 32-bit packages

--- nix/packages.nix
+++ nix/packages.nix
@@ -1,66 +1,73 @@
-pkgs: with pkgs; [
-  bash
-  coreutils
+pkgs:
 
-  # system
-  sudo
-  file
-  lsof
-  polkit
-  rsync
-  lsb-release
-  glibc_multi.static
-  dbus
-  systemd
-  glibcLocales
-  fuse2
-  openssl
-  curl
-  xz
-  python3
+let
+  basePkgsFrom =
+    pkgs: with pkgs; [
+      bash
+      coreutils
 
-  # audio
-  pulseaudio
-  openal
-  speex
-  libvorbis
-  alsa-plugins
+      # system
+      sudo
+      file
+      lsof
+      polkit
+      rsync
+      lsb-release
+      glibc_multi.static
+      dbus
+      systemd
+      glibcLocales
+      fuse2
+      openssl
+      curl
+      xz
+      python3
 
-  # multimedia
-  SDL
-  SDL2
-  libjpeg_turbo
-  harfbuzz
+      # audio
+      pulseaudio
+      openal
+      speex
+      libvorbis
+      alsa-plugins
 
-  # graphics
-  mesa
-  mesa_glu
-  libglvnd
-  glew
-  mesa-demos
-  vulkan-tools
-  libvdpau
-  libva
-  # removed from Nixpkgs (obsolete)
-  #llvm_14
-  #llvm_16
-  llvm
-  clinfo
+      # multimedia
+      SDL
+      SDL2
+      libjpeg_turbo
+      harfbuzz
 
-  # gaming
-  mangohud
-  wine # 32-bit & 64-bit on x86_64-linux, else 32-bit
+      # graphics
+      mesa
+      mesa_glu
+      libglvnd
+      glew
+      mesa-demos
+      vulkan-tools
+      libvdpau
+      libva
+      # removed from Nixpkgs (obsolete)
+      #llvm_14
+      #llvm_16
+      llvm
+      clinfo
 
-  # other
-  protobuf
-  protobufc
-  libxcb
-  xcb-util-cursor
-  xcb-imdkit
-  libxcb-image
-  libxcb-keysyms
-  libxcb-wm
-  libxkbcommon
+      # gaming
+      mangohud
+      wine # 32-bit & 64-bit on x86_64-linux, else 32-bit
 
-  # TODO: 32-bit
-]
+      # other
+      protobuf
+      protobufc
+      libxcb
+      xcb-util-cursor
+      xcb-imdkit
+      libxcb-image
+      libxcb-keysyms
+      libxcb-wm
+      libxkbcommon
+    ];
+
+  pkgs64 = basePkgsFrom pkgs;
+  pkgs32 = basePkgsFrom pkgs.pkgsi686Linux;
+in
+pkgs64 ++ pkgs32
