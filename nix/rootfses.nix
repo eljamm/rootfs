817abb301f163a98c990f220d8844710dc0a394e
eljamm <fedi.jamoussi@protonmail.ch>
2026-06-01 17:06:23 +0200

    use fhsenv

--- nix/rootfses.nix
+++ nix/rootfses.nix
@@ -11,55 +11,25 @@ let
     }:
 
     let
-      allPackages = import ./packages.nix { inherit pkgs lib type; };
-
-      rootEnv = pkgs.buildEnv {
-        name = "root";
-        paths = allPackages;
-        pathsToLink = [ "/" ];
-        ignoreCollisions = true;
+      fhsEnv = pkgs.buildFHSEnvBubblewrap {
+        name = name;
+        targetPkgs = _:
+          import ./packages.nix { inherit pkgs lib type; };
       };
     in
 
-    # Build and compress an FHS environment that follows UsrMerge:
-    # https://www.freedesktop.org/wiki/Software/systemd/TheCaseForTheUsrMerge
     pkgs.runCommand name
       {
         nativeBuildInputs = with pkgs; [
           libarchive
-          fakeroot
         ];
       }
       ''
         set -e
-        ROOTFS_DIR=$(mktemp -d)
-
-        # standard Linux directories
-        mkdir -p "$ROOTFS_DIR"/{etc,tmp,var,dev,proc,sys,run,root,home,media,mnt,opt,srv,usr}
-
-        # copy packages on top (under fakeroot for correct ownership)
-        # NOTE: symlinks are dereferenced and broken ones are ignored
-        fakeroot cp -rL "${rootEnv}/." "$ROOTFS_DIR/" || true
-
-        # Convert to usrmerge layout: move content from /{bin,lib,lib64,sbin}
-        # to /usr/{bin,lib,lib64,sbin} and replace with symlinks
-        for dir in bin lib lib64 sbin; do
-          if [ -d "$ROOTFS_DIR/$dir" ] && [ ! -L "$ROOTFS_DIR/$dir" ]; then
-            mkdir -p "$ROOTFS_DIR/usr"
-            mv "$ROOTFS_DIR/$dir" "$ROOTFS_DIR/usr/$dir"
-            ln -sf "usr/$dir" "$ROOTFS_DIR/$dir"
-          fi
-        done
-
-        # Create tar.gz with exclusions (under fakeroot for root ownership in archive)
         mkdir -p "$out"
-        ${pkgs.fakeroot}/bin/fakeroot bsdtar -czf "$out/${name}.tar.gz" \
-          --exclude=media --exclude=mnt --exclude=root --exclude=srv \
-          --exclude=boot --exclude=home --exclude=run --exclude=proc \
-          --exclude=sys --exclude=dev --exclude=tmp --exclude=.dockerenv \
-          -C "$ROOTFS_DIR" .
-
-        rm -rf "$ROOTFS_DIR"
+        bsdtar -czf "$out/${name}.tar.gz" \
+          --exclude=nix-support \
+          -C "${fhsEnv.fhsenv}" .
         echo "Done: $out/${name}.tar.gz"
       '';
 in
