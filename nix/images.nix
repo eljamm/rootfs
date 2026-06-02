87106e26e98bf2f79f155d32bcb1515246f76e87
eljamm <fedi.jamoussi@protonmail.ch>
2026-06-02 10:24:23 +0200

    more cleanup

--- nix/images.nix
+++ nix/images.nix
@@ -8,7 +8,7 @@ let
   mkImage =
     {
       name,
-      packages,
+      extraPackages,
       type,
     }:
 
@@ -21,40 +21,18 @@ let
       basePackages = import ./packages.nix {
         inherit pkgs lib type;
       };
+
+      rootEnv =
+        (pkgs.buildFHSEnvBubblewrap {
+          name = "felix86-fhs-env";
+          targetPkgs = _: basePackages ++ extraPackages;
+        }).fhsenv;
     in
 
     n2c.nix2container.buildImage {
       name = name;
       tag = "latest";
-      copyToRoot =
-        (pkgs.buildFHSEnvBubblewrap {
-          name = "felix86-fhs-env";
-          targetPkgs = _: basePackages ++ packages;
-        }).fhsenv;
-
-      # WARN: only disable when debugging, else the resulting rootfs will not
-      # be self-contained
-      # includeStorePaths = true;
-
-      # enableFakechroot = true;
-      # fakeRootCommands =
-      #   # bash
-      #   ''
-      #     # strip runtime-unnecessary files to reduce size (FHS + Nix store paths)
-      #     find . -name "*.a" -delete
-      #     find . -name "llvm-exegesis" -type f -delete
-      #     find . -path "*/python3.*/test" -type d -prune -exec rm -rf {} + 2>/dev/null || true
-      #     find . -path "*/python3.*/idlelib" -type d -prune -exec rm -rf {} + 2>/dev/null || true
-      #     find . -name "__pycache__" -type d -prune -exec rm -rf {} + 2>/dev/null || true
-      #     find . -name "*.pyc" -o -name "*.pyo" -delete
-      #     find . -path "*/share/man" -type d -prune -exec rm -rf {} + 2>/dev/null || true
-      #     find . -path "*/share/doc" -type d -prune -exec rm -rf {} + 2>/dev/null || true
-      #     find . -path "*/share/info" -type d -prune -exec rm -rf {} + 2>/dev/null || true
-      #     find . -name "locale-archive" -delete
-      #     find . -name "gsettings.xml" -delete
-      #     find . -name "gschemas.compiled" -delete
-      #   '';
-
+      copyToRoot = rootEnv;
       config = {
         entrypoint = [ "${pkgs.bash}/bin/bash" ];
       };
@@ -64,7 +42,7 @@ in
 {
   default = mkImage {
     name = "felix86";
-    packages = [ ];
     type = "full";
+    extraPackages = with pkgs; [ ];
   };
 }
