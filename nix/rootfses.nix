b074e60da55ada4e710386197b7539baae61db50
eljamm <fedi.jamoussi@protonmail.ch>
2026-06-02 15:14:35 +0200

    compose rootfs as a script instead of runcommand

--- nix/rootfses.nix
+++ nix/rootfses.nix
@@ -9,29 +9,40 @@ let
   mkTarball =
     image:
 
-    pkgs.runCommand image.imageName
-      {
-        nativeBuildInputs = with pkgs; [
-          gnutar
-          umoci
-          writableTmpDirAsHomeHook
-        ];
-      }
-      ''
+    pkgs.writeShellApplication {
+      name = image.imageName;
+      runtimeInputs = with pkgs; [
+        gnutar
+        umoci
+      ];
+      text = ''
         set -e
 
-        mkdir -p oci-layout unpacked $out
+        TMPDIR=$(mktemp -d)
 
-        # copy image to OCI layout directory
-        ${image.copyTo}/bin/copy-to oci:./oci-layout:${image.imageTag}
+        IMAGE_NAME="${image.imageName}"
+        IMAGE_TAG="${image.imageTag}"
 
-        # unpack image
-        umoci unpack --rootless --image ./oci-layout:${image.imageTag} ./unpacked
+        echo "Unpacking $IMAGE_NAME:$IMAGE_TAG image"
+        pushd "$TMPDIR"
+        mkdir -p oci-layout unpacked
 
-        # compress
-        tar -czf "$out/${image.imageName}.tar.gz" -C ./unpacked/rootfs .
+        # copy image to an OCI-layout directory
+        ${image.copyTo}/bin/copy-to oci:./oci-layout:$IMAGE_TAG
+
+        umoci unpack --rootless --image ./oci-layout:$IMAGE_TAG ./unpacked
+        popd
+
+        echo "Creating $IMAGE_NAME.tar.gz"
+        tar \
+          --owner=0 \
+          --group=0 \
+          -czf "$IMAGE_NAME.tar.gz" \
+          -C "$TMPDIR"/unpacked/rootfs \
+          .
       '';
+    };
 in
 
-# convert all OCI artefacts into compressed tarballs
+# convert all OCI artefacts into scripts that generate compressed tarballs
 lib.mapAttrs' (name: value: lib.nameValuePair (name + "-rootfs") (mkTarball value)) images
